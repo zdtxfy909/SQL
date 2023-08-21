1605310bfd3beb43d2560fdc801bf8089bb67152
chohee Lee <139777677+zdtxfy909@users.noreply.github.com>
2023-08-21 18:29:34 +0900

    Add files via upload

---
+++ 12_SEQUENCE/01_시퀀스(SEQUENCE).sql
@@ -0,0 +1,13 @@
+/* 시퀀스(SEQUENCE) ******
+SEQUENCE : DB에서 제공하는 자동채번 객체
+생성 : CREATE SEQUENCE 시퀀스명;
+삭제 : DROP SEQUENCE 시퀀스명; 
+
+시퀀스명.NEXTVAL : 시퀀스 번호(값) 생성
+시퀀스명.CURRVAL : 현재 시퀀스값 확인(NEXTVAL 한 번 이상 실행 후)
+**************************/
+
+
+
+
+
